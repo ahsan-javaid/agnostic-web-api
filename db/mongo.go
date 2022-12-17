f2a1314589c89ad135f6baadbd5fe2130387e67f
ahsan-javaid <ahsan786cs@gmail.com>
2022-12-17 10:43:41 +0500

    chore: show db connect uri

--- db/mongo.go
+++ db/mongo.go
@@ -17,8 +17,10 @@ func Connect() {
 	URI := os.Getenv("DB_URI")
 	DB_NAME := os.Getenv("DB_NAME")
 
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(fmt.Sprintf("%s/%s", URI, DB_NAME)))
+	connectionUri := fmt.Sprintf("%s/%s", URI, DB_NAME)
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connectionUri))
 	utils.Check(err)
-	log.Println("Connected to mongodb: ", URI, DB_NAME)
+	log.Println("Connected to mongodb: ", connectionUri)
   DB = client.Database(DB_NAME)
 }
